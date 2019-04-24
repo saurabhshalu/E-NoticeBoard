a93cfb97e31d0af0cf08ec4911c18380117fa865
Saurabh Verma <saurabhshalu@gmail.com>
2019-04-24 12:59:56 +0530

    fixed loading on registration

--- web/js/script.js
+++ web/js/script.js
@@ -120,7 +120,7 @@ $(document).ready(function() {
                 },
                 success: function(response)
                 {
-                  hideLoginScreen();
+                  hideLoadingScreen();
                   if(response==="success")
                       window.location.href = "./dashboard";
                   else
